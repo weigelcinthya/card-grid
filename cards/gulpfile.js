dfe6c2353e6a1a039119f40b5a1163e014131433
cinthya <cinthya.weigel@exablack.com>
2019-03-25 00:57:03 -0300

    fist commit

---
+++ cards/gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+
+const fractal = require('@frctl/fractal').create();
+
+fractal.set('project.title', 'Grid Card Component'); // title for the project
+fractal.web.set('builder.dest', 'build'); // destination for the static export
+fractal.docs.set('path', `${__dirname}/docs`); // location of the documentation directory.
+fractal.components.set('path', `${__dirname}/components`); // location of the component directory.
+
+
+const logger = fractal.cli.console;
+
+gulp.task('start', function(){
+    const server = fractal.web.server({
+        sync: true
+    });
+    server.on('error', err => logger.error(err.message));
+    return server.start().then(() => {
+        logger.success(`Fractal server is now running at ${server.url}`);
+    });
+});
+
+gulp.task('build', function(){
+    const builder = fractal.web.builder();
+    builder.on('progress', (completed, total) => logger.update(`Exported ${completed} of ${total} items`, 'info'));
+    builder.on('error', err => logger.error(err.message));
+    return builder.build().then(() => {
+        logger.success('Fractal build completed!');
+    });
+});
